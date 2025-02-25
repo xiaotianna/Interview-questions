266655ff3ec4641758409ddd8c8156f7c1744c4d
wifi歪f <1402772884@qq.com>
2025-02-25 22:04:38 +0800

    update

--- docs/.vitepress/config.ts
+++ docs/.vitepress/config.ts
@@ -7,7 +7,7 @@ export default defineConfig({
   markdown: {
     lineNumbers: true
   },
-  base: '/interview-question/',
+  base: '/Interview-questions/',
   themeConfig: {
     search: {
       provider: 'local',
